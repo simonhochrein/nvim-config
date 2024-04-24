b6b6f733a8f9c434c0cebf013f3e8cc95ccdcde8
Simon Hochrein <simon@expedition.co>
2024-04-24 16:00:30 -0500

    add snippet support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require("lsp-zero").preset({})
+local cmp_action = require("lsp-zero").cmp_action()
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Insert }
@@ -9,6 +10,8 @@ cmp.setup({
 		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 	}),
 })
 
