57a6877bbfdab6acdbc97c02865915c991dca6d9
Simon Hochrein <simon.hochrein@icloud.com>
2024-08-08 21:39:11 -0500

    Remove nvim-ufo

--- lua/simonhochrein/set.lua
+++ lua/simonhochrein/set.lua
@@ -44,3 +44,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	command = "undojoin | Neoformat",
 })
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = '1'
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  callback = function()
+    if require("nvim-treesitter.parsers").has_parser() then
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+    else
+      vim.opt.foldmethod = "syntax"
+    end
+  end,
+})
